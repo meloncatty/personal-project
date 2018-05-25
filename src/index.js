165d07a7301f817c1affbaab80f07363aab8f14a
meloncatty <k.handel66@gmail.com>
2018-05-24 22:21:30 -0600

    Set up store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App/index';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer)
+
+const app = (
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+)
+
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
