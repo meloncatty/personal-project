e677e30a233abcd7bb944358f1086ba5aa6f9ff9
meloncatty <k.handel66@gmail.com>
2018-05-26 18:40:25 -0600

    Add submit and input handler

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import {LandingPage} from '../LandingPage'
+import { LandingPage } from '../LandingPage'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        Hello
+
         <LandingPage />
       </div>
     );
--- src/LandingPage/index.js
+++ src/LandingPage/index.js
@@ -1,7 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-export const LandingPage = () => {
-  return(
-    <div>Landing Page</div>
-  )
+export class LandingPage extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      searchInput: ''
+    }
+  }
+
+  handleChange=(e)=> {
+    this.setState({
+      searchInput: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    
+  }
+
+  render() {
+    return(
+      <main>
+        <form
+          onSubmit={this.handleSubmit}>
+          <input
+            onChange={this.handleChange}
+            name='query'
+            type='text'
+            value={this.state.query}
+            placeholder='Search for journals, articles, authors, etc'
+          />
+          <button>Submit</button>
+        </form>
+      </main>
+    )
+  }
 }
+
+const mapStateToProps = state => ({
+  getUserQuery: state.getUserQuery
+})
+
+export default connect(null, mapStateToProps)(LandingPage)
