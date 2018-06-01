5897a8d640c8deb2d5dc236b0bd9d9aa288a095c
meloncatty <k.handel66@gmail.com>
2018-06-01 17:48:32 -0600

    Add styles for article container

--- src/ArticleContainer/index.js
+++ src/ArticleContainer/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
+import './styles.css'
 
 export class ArticleContainer extends Component {
   constructor() {
@@ -8,15 +9,20 @@ export class ArticleContainer extends Component {
   }
 
   render() {
-    const {authors, datePublished, fullText, title, topics} = this.props.fullArticleSuccess
+    const {authors, datePublished, fullText, title, topics, fulltextIdentifier} = this.props.fullArticleSuccess
     return (
+      this.props.fullArticleSuccess.length > 0 &&
       <section className='article-container'>
         <article className='article-contents'>
           <h2>{title}</h2>
           <h3>{authors}</h3>
           <h3>{datePublished}</h3>
-          <p>{fullText}</p>
+          <div className='toggle-text'>
+            <p>{fullText}</p>
+          </div>
           <h4>{topics}</h4>
+          {fulltextIdentifier && 
+          <a href={fulltextIdentifier}>Human friendly full text</a>}
         </article>
       </section>
     )
