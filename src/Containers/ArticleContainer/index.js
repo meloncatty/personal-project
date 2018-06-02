af0a211966ae1702e5458138ee0b1082ae15ca7f
meloncatty <k.handel66@gmail.com>
2018-06-02 13:56:13 -0600

    Add loading animation Fix logic for displaying full article text Add error text when full text fails

--- src/Containers/ArticleContainer/index.js
+++ src/Containers/ArticleContainer/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
+import loading from '../../assets/loading02.gif'
 import './styles.css'
 
 export class ArticleContainer extends Component {
@@ -8,11 +9,9 @@ export class ArticleContainer extends Component {
     super()
   }
 
-  render() {
+  cleanFullArticle = () => {
     const {authors, datePublished, fullText, title, topics, fulltextIdentifier} = this.props.fullArticleSuccess
     return (
-      this.props.fullArticleSuccess.length > 0 &&
-      <section className='article-container'>
         <article className='article-contents'>
           <h2>{title}</h2>
           <h3>{authors}</h3>
@@ -22,15 +21,50 @@ export class ArticleContainer extends Component {
           </div>
           <h4>{topics}</h4>
           {fulltextIdentifier && 
-          <a href={fulltextIdentifier}>Human friendly full text</a>}
+          <a href={fulltextIdentifier} target='target_blank'>Human friendly full text</a>}
         </article>
+    )
+  }
+
+  toggleLoading = () => {
+    if(this.props.fullArticleSuccess > 0) {
+      console.log('fuck you')
+    }
+    return this.props.fullArticleLoading
+      ? this.loadingStation()
+      : this.cleanFullArticle()
+  }
+
+  loadingStation = () => {
+    return (
+      <div className='loading-container'>
+        <img src={loading} alt='Loading icon' />
+      </div>
+    )
+  }
+
+  displayErrorText = () => {
+    return (
+      <div className='error-container'>
+        <p>Oops! Something went wrong. Please try again.</p>
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <section className='article-container'>
+      {this.toggleLoading()}
+      {this.props.fullArticleErrored && this.displayErrorText()}
       </section>
     )
   }
 }
 
 export const mapStateToProps = state => ({
-  fullArticleSuccess: state.fullArticleSuccess
+  fullArticleSuccess: state.fullArticleSuccess,
+  fullArticleErrored: state.fullArticleErrored,
+  fullArticleLoading: state.fullArticleLoading
 })
 
 export default withRouter(connect(mapStateToProps)(ArticleContainer))
