754c8e3dae376255b470119b5be1d8a95a6076be
meloncatty <k.handel66@gmail.com>
2018-06-05 01:28:48 -0600

    Partially implement fetch user articles

--- src/Containers/UserDashboard/index.js
+++ src/Containers/UserDashboard/index.js
@@ -1,46 +1,64 @@
 import React, { Component } from 'react'
 import { db } from '../../Firebase/firebase'
 import { connect } from 'react-redux'
-import { captureUserArticles } from '../../Actions'
+import { captureUserArticles, fetchUserArticles } from '../../Actions'
 import { withRouter } from 'react-router-dom'
 
 export class UserDashboard extends Component {
 
-  fetchArticles() {
+  constructor() {
+    super()
+
+    this.state = {
+      storeArticles: []
+    }
+  }
+
+  fetchArticlesFromFirebase = () => {
     const {isUserSignedIn} = this.props
     if(isUserSignedIn) {
-      // const {email} = this.props.userAuthentication[0].user
       const ref = db.ref('users').child(`${this.props.userAuthentication[0]}`).child('articles')
       ref.once('value')
         .then((snapshot) => {
           snapshot.forEach((childSnapshot) => {
             const data = childSnapshot.val()
-            this.props.captureUserArticles(data)
+            this.setState({
+              storeArticles: [data]
+            })
           })
         })
     }
   }
 
   componentDidMount() {
-    this.fetchArticles()
+    this.fetchArticlesFromFirebase()
+    this.props.fetchUserArticles(this.state.storeArticles)
+  }
+
+  fetchArticlesFromApi = () => {
+    
+    // this.state.storeArticles.forEach(articleId => this.props.fetchUserArticles(articleId))
   }
 
   render() {
     return (
-      <div>
-
-      </div>
+      <section>
+        {}
+      </section>
     )
   }
 }
 
 const mapStateToProps = state => ({
   isUserSignedIn: state.isUserSignedIn,
-  userAuthentication: state.userAuthentication
+  userAuthentication: state.userAuthentication,
+  fetchUserArticlesSuccess: state.fetchUserArticlesSuccess,
+  captureUserArticles: state.captureUserArticles
 })
 
 const mapDispatchToProps = dispatch => ({
-  captureUserArticles: (articles) => dispatch(captureUserArticles(articles))
+  captureUserArticles: (articles) => dispatch(captureUserArticles(articles)),
+  fetchUserArticles: (userArticles) => dispatch(fetchUserArticles(userArticles))
 })
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(UserDashboard))
