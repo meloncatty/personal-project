351bef8f2e6687bd0e137a07c30240c37db1edff
meloncatty <k.handel66@gmail.com>
2018-06-06 01:15:38 -0600

    Add SignOut tests

---
+++ src/Containers/SignOut/test/index.test.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+import {SignOutButton, mapDispatchToProps} from '../'
+import {isUserSignedIn} from '../../../Actions'
+
+describe('SignOut', () => {
+  it('should match snapshot', async () => {
+    const wrapper = shallow(<SignOutButton />)
+    expect(wrapper).toMatchSnapshot()
+  });
+
+  it('should sign out user', () => {
+    const userAuthentication = jest.fn()
+    const wrapper = shallow(<SignOutButton userAuthentication={userAuthentication} />)
+    wrapper.find('.sign-out').simulate('click')
+
+    expect(wrapper.instance().userAuthentication).toHaveBeenCalled
+  });
+
+  describe('mapDispatchToProps', () => {
+    it('should return false when user signs out', () => {
+      const wrapper = shallow(<SignOutButton />)
+      const mockDispatch = jest.fn()
+      const actionToDispatch = isUserSignedIn(false)
+      const mappedProps = mapDispatchToProps(mockDispatch)
+      mappedProps.isUserSignedIn(false)
+
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch)
+    });
+  });
+});
